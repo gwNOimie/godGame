8b820d7fb71633c25c23ed4e6799e9b4379e08ec
gwNO <gwenael.henot@gmail.com>
2017-12-21 11:19:45 +0100

    init db service

--- godAdmin/src/app/app.module.ts
+++ godAdmin/src/app/app.module.ts
@@ -1,11 +1,13 @@
+import { DatabaseService } from './services/database/database.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { RoutingModule } from './modules/routing/routing.module';
 
 
 @NgModule({
@@ -16,9 +18,11 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
     PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [DatabaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
