b403aa41b2df9c02d9c3f5d413b94179e96054a0
LouisGautierTransway <louis.gautier@outlook.fr>
2017-12-21 14:00:56 +0100

    getItem on models/users.js

--- godBackend/models/users.js
+++ godBackend/models/users.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 
 // model
-var player = new mongoose.Schema({
+var users = new mongoose.Schema({
   pseudo: { type: String, match: /^[a-zA-Z0-9-_]+$/ },
   password: { type: String, match: /^[a-zA-Z0-9-_]+$/ },
   email: { type: String, match: /^[a-zA-Z0-9-_]+$/ },
@@ -12,8 +12,28 @@ var player = new mongoose.Schema({
 
 });
 
+var Users = mongoose.model('users',users);
+
 // Methods
 var getList = () => {
 
+}
+
+var getItem = (res) => {
+	//db.restaurants.find({"name": sName}).pretty
+
+	Users.find({_id:'x'},function(err, result) {
+            if (err) {
+                console.log(err);
+                res.send('database error');
+                return
+            }
+            var values = {};
+            for (var i in result) {
+                var val = result[i];
+                values[val["_id"]] = val["value"]
+            }
+            res.render('index', {title: 'Geo', values: values});
+    });
 }
 
